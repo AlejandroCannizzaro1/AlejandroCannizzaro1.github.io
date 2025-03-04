459be5a577d0c0271a45fd50266555ed836f7b2c
Alejandro Cannizzaro <alejandrocannizzaro@gmail.com>
2025-03-04 18:30:05 -0300

    First-Commit

---
+++ Portfolio- Alejandro Cannizzaro/JS/script.js
@@ -0,0 +1,10 @@
+function copiarTexto(texto) {
+    navigator.clipboard.writeText("alejandrocannizzaro@gmail.com").then(() => {
+        alert("Correo copiado: " + texto);
+    }).catch(err => {
+        console.error("Error al copiar", err);
+    });
+}
+
+/* ✅ navigator es un objeto que permite acceder a funcionalidades del navegador y del dispositivo del usuario.
+✅ Se usa para cosas como copiar texto, obtener la ubicación, saber si hay conexión, acceder a la batería, etc.*/
